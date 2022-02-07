54b890e4f38ac84e982a8825b9353d1b364c46c3
Adam Lindgren <adam@adamlindgren.com>
2022-02-07 16:09:33 -0500

    all info on page, CSS next

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-let mainImg = document.getElementsByClassName("image").src
 
-customers.map( customer => {
-    mainImg = customers[0].picture.thumbnail
-})
+
+console.log("hello, world")
+const customer = customers[0]
+const personDiv = document.getElementById("person")
+
+
+
+//for loop (maybe map)
+for (let customer of customers){
+    //show image for each person
+    let personName = `${customer.name.first} ${customer.name.last}`
+    let thumbnailURL = customer.picture.thumbnail
+    personDiv.innerHTML += `<img src=${thumbnailURL} alt="photo of ${customer.name.first}">`
+
+    //need name of each person
+    personDiv.innerHTML += `<h2>${personName}</h2>`
+
+    //email address
+    personDiv.innerHTML += `<p class="email">${customer.email}</p>`
+
+    //address
+    personDiv.innerHTML += `
+    <p class="otherInfo">${customer.location.street.number} ${customer.location.street.name}</p>
+    <p class="otherInfo">${customer.location.city}, ${customer.location.state} ${customer.location.postcode}
+    `
+
+    //DOB
+    personDiv.innerHTML += `<p class="otherInfo">DOB: ${moment(customer.dob.date).format("MMM DD, YYYY")}</p>`
+
+    //Customer Since
+    personDiv.innerHTML += `<p class="otherInfo">Customer since: ${moment(customer.registered.date).format("MMM DD, YYYY")}</p>`
+}
